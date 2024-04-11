79e64401a1a89b6f56e5ac426c7267d0f772cfbb
Grace O. Nweke <lagra2001@gmail.com>
2024-04-11 20:58:44 +0000

    solving for filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,7 +10,15 @@ def filter_datum(fields, redaction, message, separator):
     """
     a function called filter_datum that returns the log message obfuscated:
     """
+    """
     for field in fields:
         mesage = re.sub(f'{field}=.*?{separator}',
                         f'{field}={redaction}{separator}', message)
     return message
+    """
+    pattern = '|'.join(map(re.escape, fields))
+    return re.sub(pattern, redaction, message).replace(f'{separator}{separator}', separator)
+
+
+if __name__ == "__main__":
+    main()
