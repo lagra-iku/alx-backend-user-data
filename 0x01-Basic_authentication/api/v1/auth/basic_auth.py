5f05a0a300f8c9a13fe68bef9ea1747f96f0f235
Grace O. Nweke <lagra2001@gmail.com>
2024-04-16 22:22:31 +0000

    updating basic auth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,14 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Class Basic Auth
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """extract_base64_authorization_header"""
+        if (authorization_header is None or
+                not isinstance(authorization_header, str)):
+            return None
+
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header.split(' ')[1]
