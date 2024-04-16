3429d94988ed9d551675c6187697c90a4bdfdd37
Grace O. Nweke <lagra2001@gmail.com>
2024-04-16 22:03:24 +0000

    adding request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,40 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+AUTH_TYPE = os.getenv("AUTH_TYPE")
+if AUTH_TYPE:
+    if AUTH_TYPE == "basic_auth":
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
+    elif AUTH_TYPE == "session_auth":
+        from api.v1.auth.session_auth import SessionAuth
+        auth = SessionAuth()
+
+
+@app.before_request
+def before_request_handler():
+    """ Request validation!
+    """
+    if auth is None:
+        return
+
+    excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/'
+            ]
+    if request.path in excluded_paths:
+        return
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 @app.errorhandler(404)
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -25,7 +25,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Method authorization header"""
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Method current user"""
