4e49b02e9ebc058f666658c0811ae2b627601d84
Grace O. Nweke <lagra2001@gmail.com>
2024-04-16 22:27:16 +0000

    adding decode method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Module of the Basic Auth Class
 """
 
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -19,3 +20,17 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """decode_base64_authorization_header"""
+        if (base64_authorization_header is None or
+                not isinstance(base64_authorization_header, str)):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except Exception:
+            return None
