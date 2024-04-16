696e3e20ca9130fd77cac72e0f46ded91f0e2b6c
Grace O. Nweke <lagra2001@gmail.com>
2024-04-16 21:57:16 +0000

    adding auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,16 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Method require_auth"""
-        return False
+        if path is None or not excluded_paths:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if path == excluded_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Method authorization header"""
