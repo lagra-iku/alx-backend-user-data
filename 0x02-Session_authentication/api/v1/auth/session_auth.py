0d348e48dc8584626c2e50d939feee2d2c6418b3
Grace O. Nweke <lagra2001@gmail.com>
2024-04-18 14:10:12 +0000

    solving for user id for session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -18,3 +18,9 @@ class SessionAuth(Auth):
             session_id = str(uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """method for User ID for Session ID.
+        """
+        if type(session_id) is str:
+            return self.user_id_by_session_id.get(session_id)
