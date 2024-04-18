44c674137f1a64e111526199b21519b63e6d52f7
Grace O. Nweke <lagra2001@gmail.com>
2024-04-18 14:12:22 +0000

    solving for session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -35,3 +35,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Method current user"""
         return None
+
+    def session_cookie(self, request=None) -> str:
+        """method for session cookie
+        """
+        if request is not None:
+            cookie_name = os.getenv('SESSION_NAME')
+            return request.cookies.get(cookie_name)
