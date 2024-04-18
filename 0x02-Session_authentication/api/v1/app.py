f15c7d228709075cab293a601dab3807e67ffd15
Grace O. Nweke <lagra2001@gmail.com>
2024-04-18 14:23:58 +0000

    adding session cookie

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -42,7 +42,8 @@ def before_request_handler():
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None
+            and auth.session_cookie(request) is None):
         abort(401)
 
     if auth.current_user(request) is None:
