8482a0a043a13a05518f1cc2fa61d4cced663361
Grace O. Nweke <lagra2001@gmail.com>
2024-04-25 13:55:39 +0000

    solving for task 20

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,70 +1,53 @@
 #!/usr/bin/env python3
-
 """
-End-to-end integration test
+End-to-end integration test module
 """
 
 
 def register_user(email: str, password: str) -> None:
-    """
-    register_user.
-    """
+    """register_user method"""
     assert True
     return
 
 
 def log_in_wrong_password(email: str, password: str) -> None:
-    """
-    log_in_wrong_password.
-    """
+    """log_in_wrong_password method"""
     assert True
     return
 
 
 def log_in(email: str, password: str) -> str:
-    """
-    log_in.
-    """
+    """log_in method"""
     assert True
     return ""
 
 
 def profile_unlogged() -> None:
-    """
-    profile_unlogged.
-    """
+    """profile_unlogged method"""
     assert True
     return
 
 
 def profile_logged(session_id: str) -> None:
-    """
-    profile_logged.
-    """
+    """profile_logged method"""
     assert True
     return
 
 
 def log_out(session_id: str) -> None:
-    """
-    log_out.
-    """
+    """log_out method"""
     assert True
     return
 
 
 def reset_password_token(email: str) -> str:
-    """
-    reset_password_token.
-    """
+    """reset_password_token"""
     assert True
     return ""
 
 
 def update_password(email: str, reset_token: str, new_password: str) -> None:
-    """
-    update_password.
-    """
+    """update_password"""
     assert True
     return
 
